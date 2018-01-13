be6a959a020a0510cdd6a05ee931d121af87d13a
KazutoSmith <raimbek.riko@gmail.com>
2018-01-13 21:34:58 +0600

    Developing login (50%) :)

--- src/com/se/cgapi/Service.java
+++ src/com/se/cgapi/Service.java
@@ -1,22 +1,56 @@
 package com.se.cgapi;
 
 
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.MediaType;
+
+import com.google.gson.JsonObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.google.gson.Gson;
+import org.bson.Document;
+import org.xml.sax.helpers.DefaultHandler;
+
 
 @Path("/v1")
 @ApplicationPath("/api")
 public class Service extends Application{
 
+    //Gson
+    private Gson gson = new Gson();
+
+    //Database Connection
+    private MongoClient mClient = new MongoClient("s247007.mlab.com", 47007);
+    private MongoDatabase db = mClient.getDatabase("se-cardgame");
+    private MongoCredential credential = MongoCredential.createCredential("admin", "se-cardgame", "Zxc123654".toCharArray());
+
+    //Collections
+    private MongoCollection<Document> USERS = db.getCollection("users");
+
+    //Test operation
     @GET
     @Path("/helloWorld")
     @Produces(MediaType.APPLICATION_JSON)
-    public String helloWorld(){
-        return "Hello World";
+    public JsonObject helloWorld(){
+
+        JsonObject result = new JsonObject();
+
+        result.addProperty("ok", true);
+
+        return result;
+    }
+
+    //Login
+    @POST
+    @Path("/login")
+    public String login(@QueryParam("username") String uname, @QueryParam("pword") String password){
+
+
+        return "ok";
     }
 
 }
